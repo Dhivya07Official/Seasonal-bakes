a6634356622a268e2c167d44e437b618a0ac1da8
Gowtham M <gowthammohan3004official@gmail.com>
2024-03-04 23:39:40 +0530

    name change

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
     <Layout>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>Seasonal bakes</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
